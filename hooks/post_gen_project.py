686b9e4944ee6000c273d0e949ef295981bdcf8f
rraadd88 <rraadd_8@hotmail.com>
2019-08-01 14:45:38 -0400

    update

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,18 +3,6 @@ import subprocess
 ## 1 make 2 analysis directories
 subprocess.call("cookiecutter --no-input https://github.com/rraadd88/template_code_analysis.git",shell=True)
 
-## soft links in all
-# dir2ps=["{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}",
-# '00_something'
-# ]
-
-# dir1ps=["{{cookiecutter.databasep}}","{{cookiecutter.depsp}}"]
-# for dir1p in dir1ps:
-#     if exists(dir1p):
-#         print(dir1p)
-#         for dir2p in dir2ps:
-#             subprocess.call(f"ln -s {dir1p} {dir2p}",shell=True)
-
 dir2ps=['data_merge','data_annot']
 for dir1p in dir2ps:
     subprocess.call(f"ln -s {{cookiecutter.repo_name}}/{dir1p} 00_something",shell=True)
